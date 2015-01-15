2068de2f064cc4b981aa823557b2717cc5c8feb9
nptomlin <neil@cloudconcept.co.uk>
2015-01-15 14:35:01 +0000

    update password

--- src/Accounts/Controllers/AccountController.cs
+++ src/Accounts/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Net.Http;
+using System.Web.Mvc;
 
 namespace Accounts.Controllers
 {
@@ -11,9 +15,31 @@ namespace Accounts.Controllers
         }
 
         [Authorize]
-        public ActionResult Reset()
+        [HttpPost]
+        public ActionResult UpdatePassword(string password)
         {
-            return View();
+            //validate password check password satisfies the complexity rules
+
+            //call api to reset the password
+            //get the current user
+
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("password", "password")
+            };
+
+            var content = new FormUrlEncodedContent(pairs);
+
+            var client = new HttpClient { BaseAddress = new Uri("https://core-api.azurewebsites.net") };
+
+            // call sync
+            var response = client.PutAsync("/reset/1", content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return Json(new {success = true});
+            }
+            Response.StatusCode = (int)response.StatusCode;
+            return Json(new { success = false });
         }
     }
 }
